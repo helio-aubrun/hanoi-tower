113ac21261ea54c47113ac4c86054441a64c42bd
helio-aubrun <helio.aubrun@laplateforme.io>
2024-07-16 13:56:37 +0200

    stock the soluce in a list

--- solve.py
+++ solve.py
@@ -1,31 +1,39 @@
-def hanoi(n, source, target, auxiliary):
-    """
-    Solves the Hanoi Tower problem with n discs between three sticks.
-    
-    Args:
-    n (int): Number of disks.
-    source (str): Source stick.
-    target (str): Target stick.
-    auxiliary (str): Auxiliary stick.
-    """
-    if n > 0:
-        # Moves n-1 disks from source to auxiliary, through target
-        hanoi(n-1, source, auxiliary, target)
-        
-        # Moves the remaining disk from source to target
-        print(f"{source} -> {target}")
-        
-        # Moves n-1 disks from auxiliary to target, through source
-        hanoi(n-1, auxiliary, target, source)
+class hanoi:
 
-# Main function to read user input and call hanoi function
-def main():
-    # Read user entry
-    input_str = input("Enter the number of disks followed by the number of sticks separated by a comma: ")
-    disks, pegs = map(int, input_str.split(','))
-    
-    # Call hanoi function with appropriate settings
-    hanoi(disks, '1', '2', '3')
+    def __init__(self):
+        self.path = []
+
+    def hanoi_solve(self, n, source, target, auxiliary):
+        """
+        Solves the Hanoi Tower problem with n discs between three sticks.
+        
+        Args:
+        n (int): Number of disks.
+        source (str): Source stick.
+        target (str): Target stick.
+        auxiliary (str): Auxiliary stick.
+        """
+        if n > 0:
+            # Moves n-1 disks from source to auxiliary, through target
+            self.hanoi_solve(n-1, source, auxiliary, target)
+            
+            # Moves the remaining disk from source to target
+            print(f"{source} -> {target}")
+            self.path.append([int(source),int(target)])
+            
+            # Moves n-1 disks from auxiliary to target, through source
+            self.hanoi_solve(n-1, auxiliary, target, source)
+        
+
+    # Main function to read user input and call hanoi function
+    def main(self):
+        # Read user entry
+        input_str = input("Enter the number of disks followed by the number of sticks separated by a comma: ")
+        disks, pegs = map(int, input_str.split(','))
+        
+        # Call hanoi function with appropriate settings
+        self.hanoi_solve(disks, '1', '2', '3')
 
 if __name__ == "__main__":
-    main()
+    main = hanoi()
+    main.main()
